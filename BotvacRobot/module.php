db5e2e4b645fbee041b3724cecd60b28a324e429
Markus K <markus.kromberg@gmail.com>
2020-03-01 19:47:16 +0100

    Update module.php

--- BotvacRobot/module.php
+++ BotvacRobot/module.php
@@ -349,7 +349,7 @@ class BotvacRobot extends IPSModule
         }
 
 		$i = 0;
-        foreach ($zones_array['boundaries'] as $zone) {
+        foreach ($zones_array['data']['boundaries'] as $zone) {
 			$i++;
 			if ($zone['name'] <> "")
 			{
